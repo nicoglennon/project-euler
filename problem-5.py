f15cb44c2ec4816fd639f99b66b59ca42d59e4e4
Nico Glennon <nicoglennon@icloud.com>
2017-09-02 15:21:56 -0500

    Clarify comments in P5

--- problem-5.py
+++ problem-5.py
@@ -6,8 +6,6 @@
 
 
 def find_smallest_multiple():
-    # create list of divisors
-
     # create the divisor list but exclude 1 thru 10 - if integers are divisible by 11-20, they will be divisible by them as well
     divisors = [11,12,13,14,15,16,17,18,19,20]
     # starting multiple is 20 - must be at least this to be divisible by 20
